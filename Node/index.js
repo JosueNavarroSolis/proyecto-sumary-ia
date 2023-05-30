8b82c184e2d5c2b3a0f06c2f0bf8e266f72c6e13
JosueNavarroSolis <56096921+JosueNavarroSolis@users.noreply.github.com>
2023-05-30 04:50:30 -0600

    Update index.js

--- Node/index.js
+++ Node/index.js
@@ -1,23 +1,22 @@
 const express = require("express")
 const app = express()
-//const PORT = 3001
-//const traductor = require('./controlador/traductor')
-//const cors = require('cors')
-//app.use(cors())
+const PORT = 3001
+const traductor = require('./controlador/traductor')
+const cors = require('cors')
+app.use(cors())
 
-//app.use(express.json({limit: "50mb"}))
-//app.use('/traductor',traductor)
-app.use(express.json());
+app.use(express.json({limit: "50mb"}))
+app.use('/traductor',traductor)
 
 app.get('/',(req,res) =>{
   res.send('aja');
 })
 
-/*app.listen(PORT, () =>{
+app.listen(PORT, () =>{
 	console.log(`Server is listening at http://localhost:${PORT}`)
 })
 
-
+/*
 const express = require('express');
 const { Translate } = require('node-google-translate-skidz');
 
